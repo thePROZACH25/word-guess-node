f85a5ac96e5f32ac420639b91d260ba3baaa0a3a
Zach S.L <zach.s.ledford@gmail.com>
2020-04-20 16:56:48 -0400

    added word.js function

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+var Word = require("./Word.js");
 var inquirer = require("inquirer");
 
+var wordArray = ["PASS JUDGMENT", "CONGRATULATIONS", "QUALITY", "RALLY", "TONED DOWN", "PROVE", "UNSURPASSED"];
+var letters = /[a-zA-Z]/;
+
+var numGuesses;
+
+function playGame(){
+    
+}
--- word.js
+++ word.js
@@ -1,2 +1,36 @@
 var Letter = require("./Letter.js");
 
+var Word = function(word){
+    this.lettersArr = [];
+
+    for (var i = 0; i < word.length; i++){
+        if (word.charAt(i) === " "){
+            this.lettersArr.push(" ");
+        }else{
+            this.lettersArr.push(new Letter (word.charAt(i)));
+        };
+    };
+
+    this.createString = function(){
+        var wordString = "";
+
+        this.lettersArr.forEach(function(element){
+            if (elemnt === " "){
+                wordString += "  ";
+            }else{
+                wordString += element.displayLetter();
+            }
+        });
+        return wordString;
+    }
+
+    this.checkGuessWord = function (letterGuess){
+        this.lettersArr.forEach(function(element){
+            if (element.letter !== undefined){
+                element.checkGuess(letterGuess);
+            }
+        });
+    };
+};
+
+module.exports = Word;
